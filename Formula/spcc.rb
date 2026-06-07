98156507dde145a955a9116371d539940079b820
Hareem Adderley <hadderley@kingpinapps.com>
2026-06-07 14:44:48 -0500

    spcc: drop redundant version, fix line order

--- Formula/spcc.rb
+++ Formula/spcc.rb
@@ -3,7 +3,6 @@ class Spcc < Formula
   homepage "https://github.com/Kingpin-Apps/swift-package-compat-check"
   url "https://github.com/Kingpin-Apps/swift-package-compat-check/releases/download/0.4.0/spcc-0.4.0-macos-universal.tar.gz"
   sha256 "0da4b0dc4d2813f762886e49891701246f6ab61af812514569595414e0bb8a81"
-  version "0.4.0"
   license "Apache-2.0"
 
   depends_on macos: :sequoia
