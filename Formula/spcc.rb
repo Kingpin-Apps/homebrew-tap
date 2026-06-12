189fda5357e5df49bac1adb68178a403a8cbf487
Hareem Adderley <HADDERLEY@KINGPINAPPS.COM>
2026-06-12 20:46:46 +0000

    spcc 0.5.3

--- Formula/spcc.rb
+++ Formula/spcc.rb
@@ -1,8 +1,8 @@
 class Spcc < Formula
   desc "Run a swift-package-index-style compatibility matrix locally"
   homepage "https://github.com/Kingpin-Apps/swift-package-compat-check"
-  url "https://github.com/Kingpin-Apps/swift-package-compat-check/releases/download/0.5.2/spcc-0.5.2-macos-universal.tar.gz"
-  sha256 "3b65265af7fc8f8a373e6be61596f4b4dd7362f0fa6876d606171b8e98cac74b"
+  url "https://github.com/Kingpin-Apps/swift-package-compat-check/releases/download/0.5.3/spcc-0.5.3-macos-universal.tar.gz"
+  sha256 "6c2036ece7fb19a0610195ca5ec282586ff679d2606277bb8d7aded725c5fd65"
   license "MIT"
 
   depends_on macos: :sequoia
