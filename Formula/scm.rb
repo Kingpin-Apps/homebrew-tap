bb90c31d33fedc90ce4f9b26c7fe4a426cc87d2d
Hareem Adderley <HADDERLEY@KINGPINAPPS.COM>
2026-09-15 02:51:39 +0000

    scm 0.9.0

--- Formula/scm.rb
+++ Formula/scm.rb
@@ -1,8 +1,8 @@
 class Scm < Formula
   desc "TUI for Cardano blockchain interactions"
   homepage "https://github.com/Kingpin-Apps/swift-cardano-multitool"
-  url "https://github.com/Kingpin-Apps/swift-cardano-multitool/releases/download/0.8.3/scm-0.8.3-macos-universal.tar.gz"
-  sha256 "b50ccf6ad6c6f6664d7405ba8504df40773cc8969c0f7f54477b22fece3543c4"
+  url "https://github.com/Kingpin-Apps/swift-cardano-multitool/releases/download/0.9.0/scm-0.9.0-macos-universal.tar.gz"
+  sha256 "0b0554aa19dfeb5af828243f41e709b02421545c2fc747f066987ac37ecfd99b"
   license "MIT"
 
   depends_on macos: :sequoia
